1e113a81602a9457abc0ef0830e77676492c65ae
M$ <19026937+mdolr@users.noreply.github.com>
2020-10-06 20:09:13 +0200

    implement i18n in the settings popup

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -8,6 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
         return link.replace('http://', '').replace('https://', '').split('/')[0].split('.').slice(subdomains - 2, subdomains).join('.');
     }
 
+    ['pageSettings', 'generalSettings', 'allowMetadata', 'enableOnPage'].forEach(function (word) {
+        document.getElementById(word).innerText = chrome.i18n.getMessage(word);
+    });
+
     chrome.storage.local.get(['disabledDomains', 'previewMetadata'], function (res) {
         let disabledDomains = res.disabledDomains ? res.disabledDomains : ['survol.me'];
         let previewMetadata = true;
@@ -22,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('currentURL').innerText = CURRENT_URL;
 
                 if (disabledDomains.includes(CURRENT_URL.toLowerCase())) {
-                    document.getElementById('pageSettings').checked = false;
+                    document.getElementById('previewOnThisPage').checked = false;
                 }
 
                 document.getElementById('previewMetadata').checked = previewMetadata;
@@ -31,14 +35,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     chrome.storage.local.set({ previewMetadata: document.getElementById('previewMetadata').checked });
                 });
 
-                document.getElementById('pageSettings').addEventListener('click', () => {
+                document.getElementById('previewOnThisPage').addEventListener('click', () => {
                     // if the box gets unchecked i.e domain disabled, and the domain is not already in the list add it
-                    if (!document.getElementById('pageSettings').checked && !disabledDomains.includes(CURRENT_URL.toLowerCase())) {
+                    if (!document.getElementById('previewOnThisPage').checked && !disabledDomains.includes(CURRENT_URL.toLowerCase())) {
                         disabledDomains.push(CURRENT_URL.toLowerCase());
                     }
 
                     // If the box gets checked and the domain is disabled, remove it from the disabled domains list
-                    else if (document.getElementById('pageSettings').checked && disabledDomains.includes(CURRENT_URL.toLowerCase())) {
+                    else if (document.getElementById('previewOnThisPage').checked && disabledDomains.includes(CURRENT_URL.toLowerCase())) {
                         disabledDomains = disabledDomains.filter((domains) => { return domains != CURRENT_URL.toLowerCase(); });
                     }
 
