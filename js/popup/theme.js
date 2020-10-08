2c4b3caa13e024a1099c5052d93c956000ad7a51
ayushi_keshri <ayushi.keshri@nearex.com>
2020-10-08 20:24:15 +0530

    rotation of moon sun fixed

--- js/popup/theme.js
+++ js/popup/theme.js
@@ -1,3 +1,9 @@
+const sunMoonContainer = document.querySelector(".sun-moon-container");
+
 document.getElementById("btn").addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  // const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue("--rotation"));
+
+  // sunMoonContainer.style.setProperty("--rotation", currentRotation + 180);
+  // console.log("rotation", sunMoonContainer);
 });
